4b632e856d69e76b7bcd64dfe701535e4877d8a0
EstherRaeth <86475520+EstherRaeth@users.noreply.github.com>
2023-11-01 15:44:16 +0100

    Add files via upload

---
+++ downloads/software/image_acquisition/openSIM_software_withoutCameraControl_V2_1/cmd-file.bat
@@ -0,0 +1,2 @@
+cd "C:/Program Files/MetroCon-3.3/RepTools"
+call RepSender.exe -d  0175000548 -z C:\Users\adminLBNI.INTRANET\Desktop\openSIM\20231011_openSIM_labview_withoutCameraControl\drivers\patterns_openSIM_all_20180802.repz -r
